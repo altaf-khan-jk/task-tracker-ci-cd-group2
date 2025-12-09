f7bd9b931ccef21f29909c9186f175b1be2985b9
Altaf Khan <Akhan8508@conestogac.on.ca>
2025-12-09 02:38:39 -0500

    updated files in one repo

--- infra/main.tf
+++ infra/main.tf
@@ -16,6 +16,7 @@ data "template_file" "user_data" {
   }
 }
 
+
 resource "aws_security_group" "web_sg" {
   name = "${var.project_name}-sg"
 
--- infra/user_data.sh
+++ infra/user_data.sh
@@ -1,15 +1,50 @@
 #!/bin/bash
 set -xe
 
-apt-get update -y
-apt-get install -y curl git unzip
+# Update system
+sudo apt-get update -y
 
-curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
-apt-get install -y nodejs
+# Install tools
+sudo apt-get install -y curl git unzip
 
+# Install Node.js 18
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Clone GitHub repo
 cd /opt
-git clone ${GITHUB_REPO_URL} app || (cd app && git pull)
+sudo git clone "${GITHUB_REPO_URL}" app || (cd app && sudo git pull)
 cd app
-npm install
 
-nohup npm start > app.log 2>&1 &
+# Install dependencies
+sudo npm install
+
+# Start app
+sudo nohup npm start > /opt/app.log 2>&1 &
+
+# Install CloudWatch agent
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i amazon-cloudwatch-agent.deb
+
+# Create CloudWatch config
+sudo tee /opt/aws/amazon-cloudwatch-agent.json > /dev/null <<EOF
+{
+  "logs": {
+    "logs_collected": {
+      "files": {
+        "collect_list": [
+          {
+            "file_path": "/opt/app.log",
+            "log_group_name": "/task-tracker-ci-cd/app",
+            "log_stream_name": "{instance_id}"
+          }
+        ]
+      }
+    }
+  }
+}
+EOF
+
+# Start CloudWatch agent
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+  -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent.json -s
